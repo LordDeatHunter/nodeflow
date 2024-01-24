3330f7d3f093b94e08929fadc7a4e6a5db488e03
LordDeatHunter <desuhansa@gmail.com>
2024-01-24 16:58:10 +0100

    Remove 'any' types in build

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,9 @@
 import withSolid from "rollup-preset-solid";
+import typescript from "rollup-plugin-typescript2";
 
 export default withSolid([
   {
+    plugins: [typescript()],
     input: "src/index.ts",
     targets: ["esm", "cjs"],
   },
