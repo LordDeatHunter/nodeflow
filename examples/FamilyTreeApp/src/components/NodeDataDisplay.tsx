70b3fdc3caaf1aa92a4890589f59b836d965a3a7
LordDeatHunter <desuhansa@gmail.com>
2024-02-28 23:59:04 +0100

    Display children in family tree example

--- examples/FamilyTreeApp/src/components/NodeDataDisplay.tsx
+++ examples/FamilyTreeApp/src/components/NodeDataDisplay.tsx
@@ -1,18 +1,25 @@
-import { Component, createMemo } from "solid-js";
+import { Component, createMemo, For, Show } from "solid-js";
 import formStyle from "../styles/form.module.scss";
 import NodeflowNodeData from "nodeflow-lib/src/utils/data/NodeflowNodeData";
 
 const NodeDataDisplay: Component<{ nodeData: NodeflowNodeData }> = (props) => {
-  const connectors = createMemo(() =>
+  const parentNodes = createMemo(() =>
     props.nodeData
       .getAllSourceConnectors()
       .map((connector) => connector.parentNode),
   );
   const mother = createMemo(() =>
-    connectors().find((node) => node.customData.gender === "F"),
+    parentNodes().find((node) => node.customData.gender === "F"),
   );
   const father = createMemo(() =>
-    connectors().find((node) => node.customData.gender === "M"),
+    parentNodes().find((node) => node.customData.gender === "M"),
+  );
+
+  const children = createMemo(() =>
+    props.nodeData
+      .getAllDestinationConnectors()
+      .map((connector) => connector.parentNode)
+      .sort((a, b) => a.customData.name.localeCompare(b.customData.name)),
   );
 
   return (
@@ -67,6 +74,26 @@ const NodeDataDisplay: Component<{ nodeData: NodeflowNodeData }> = (props) => {
             {father()?.customData.name || "Unknown"}
           </p>
         </div>
+      </div>
+      <div class={formStyle.childrenDisplayContainer}>
+        <p>Children</p>
+        <Show when={children()?.length > 0} fallback={<p>None</p>}>
+          <div class={formStyle.childrenDisplayWindow}>
+            <For each={children()}>
+              {(child) => (
+                <p
+                  onClick={() => {
+                    child.nodeflow.mouseData.clearSelections();
+                    child.select();
+                  }}
+                  class={formStyle.child}
+                >
+                  {child.customData.name}
+                </p>
+              )}
+            </For>
+          </div>
+        </Show>
       </div>
     </div>
   );
